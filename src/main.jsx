52727d53fbbe9c462fe91d7ba5be0e0c79472be4
thashreef <chthashreef22@gmail.com>
2024-08-02 19:03:40 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { SnackbarProvider } from 'notistack';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
+)
